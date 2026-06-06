dec66e658644c26d3b8713a378ec834391d18c4d
lokeshwaranraja98-ai <lokeshwaranraja98@gmail.com>
2026-06-06 11:55:27 +0530

    Delete accessible-hiring-main directory

--- accessible-hiring-main/src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/ai-voice-interview-flow.ts';
-import '@/ai/flows/ai-assessment-question-generation-flow.ts';
-import '@/ai/flows/ai-text-interview-flow.ts';
